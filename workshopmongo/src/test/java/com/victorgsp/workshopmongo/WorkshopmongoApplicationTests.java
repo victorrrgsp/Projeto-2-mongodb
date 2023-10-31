72bbb354888a808bccbbe2a5a7c60cfee4c1ad31
Victorgsp <victorgabrielpimenta52@gmail.com>
2023-10-31 15:32:03 -0300

    Projeto utilizando o spring boot e o jp

---
+++ workshopmongo/src/test/java/com/victorgsp/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorgsp.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
