dd485252aef95a9c48d3ff1bb88c45d70fb056f3
Victorgsp <victorgabrielpimenta52@gmail.com>
2023-11-07 17:41:22 -0300

    Decimo commit - Criando entity Post com User alinhado

---
+++ workshopmongo/src/main/java/com/victorgsp/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.victorgsp.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.victorgsp.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
